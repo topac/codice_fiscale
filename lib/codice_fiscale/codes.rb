84a3901ff3f3fa6e7a8b3e5cde19d5bcc23e7183
topac <topac@users.noreply.github.com>
2012-12-02 13:04:43 +0100

    calculate the check character

--- lib/codice_fiscale/codes.rb
+++ lib/codice_fiscale/codes.rb
@@ -41,4 +41,9 @@ module CodiceFiscale
       decode[character.upcase]
     end
 
+    def check_character number
+      decode = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]
+      decode[number]
+    end
+  end
 end
