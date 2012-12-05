0723a54c2736023cd68eb4e455b541678f997646
topac <topac@users.noreply.github.com>
2012-12-05 21:01:47 +0100

    move helper methods in a separate module

--- lib/codice_fiscale.rb
+++ lib/codice_fiscale.rb
@@ -6,8 +6,10 @@ require 'codice_fiscale/version'
 require 'codice_fiscale/alphabet'
 require 'codice_fiscale/codes'
 require 'codice_fiscale/configuration'
+require 'codice_fiscale/string_helper'
 
 module CodiceFiscale
+  include StringHelper
   extend self
 
   def calculate params
@@ -72,38 +74,12 @@ module CodiceFiscale
     end
   end
 
+  # todo - rename to control_character
   def check_character partial_fiscal_code
     numeric_value = 0
     partial_fiscal_code.split('').each_with_index do |chr, index|
-      numeric_value += ((index+1) % 2 == 0) ? Codes.even_character(chr) : Codes.odd_character(chr)
+      numeric_value += (index+1).even? ? Codes.even_character(chr) : Codes.odd_character(chr)
     end
     Codes.check_character numeric_value % 26
-  end
-
-
-  # Helper methods
-
-  # Intersect two strings or a string and an array of characters.
-  def intersects string_a, string_or_array_b
-    letters_a = string_a.split ''
-    letters_b = string_or_array_b.respond_to?(:split) ? string_or_array_b.split('') : string_or_array_b
-    selected_letters = letters_a.select { |letter| letters_b.include? letter }
-    selected_letters.join ''
-  end
-
-  def consonants string
-    intersects string, Alphabet.consonants
-  end
-
-  def first_three_consonants string
-    intersects(string, Alphabet.consonants)[0..2]
-  end
-
-  def first_three_vowels string
-    intersects(string, Alphabet.vowels)[0..2]
-  end
-
-  def truncate_and_right_pad_with_three_x string
-    string[0..2].ljust 3, 'X'
   end
 end
---
+++ lib/codice_fiscale/string_helper.rb
@@ -0,0 +1,30 @@
+require 'codice_fiscale/alphabet'
+
+module CodiceFiscale
+  module StringHelper
+    # Intersect two strings or a string and an array of characters.
+    # todo - rename to intersection
+    def intersects string_a, string_or_array_b
+      letters_a = string_a.split ''
+      letters_b = string_or_array_b.respond_to?(:split) ? string_or_array_b.split('') : string_or_array_b
+      selected_letters = letters_a.select { |letter| letters_b.include? letter }
+      selected_letters.join ''
+    end
+
+    def consonants string
+      intersects string, Alphabet.consonants
+    end
+
+    def first_three_consonants string
+      intersects(string, Alphabet.consonants)[0..2]
+    end
+
+    def first_three_vowels string
+      intersects(string, Alphabet.vowels)[0..2]
+    end
+
+    def truncate_and_right_pad_with_three_x string
+      string[0..2].ljust 3, 'X'
+    end
+  end
+end
